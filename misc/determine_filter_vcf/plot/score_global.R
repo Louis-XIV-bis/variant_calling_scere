2396dc3b1f767a04fa674dc9105767e1630c0f6e
Louis-XIV-bis <tigerwhite27@hotmail.fr>
2024-09-12 13:50:28 +0200

    new filter for RankSum score

--- misc/determine_filter_vcf/plot/score_global.R
+++ misc/determine_filter_vcf/plot/score_global.R
@@ -33,17 +33,17 @@ annotations <- read.table("../results/table_scores_merged.tsv", sep="\t", header
 
 head(annotations)
 
-# Number of NA removed 
+# remove the NA only for the scores that do not require heterozygous SNP
 rows_before <- nrow(annotations)
 annotations <- annotations %>%
-  drop_na()
+  drop_na(-c(MQRankSum, ReadPosRankSum, BaseQRankSum))
 rows_after <- nrow(annotations)
 cat("Number of rows before dropping NA:", rows_before, ", after: ", rows_after, "\nTotal removed: ", rows_before-rows_after)
 
-# Group by CHROM and count the number of SNP for each group
 rows_per_chrom <- annotations %>%
   group_by(CHROM) %>%
   summarize(n = n())
+print("Number of SNP for each chromosome after drop_na:")
 print(rows_per_chrom)
 
 ########################################################################
@@ -59,33 +59,44 @@ lim_sup.ReadPosRankSum = 5.0
 lim.SOR = 2
 lim.BaseQRankSum = -2
 
-# Calculate the proportion of rows that did not pass each filter
+# Calculate the proportion of rows that did not pass each filter, considering NAs
 proportions_pass <- annotations %>%
   summarize(
-    pass_QD = round(mean(QD > lim.QD), 3),
-    pass_FS = round(mean(FS < lim.FS), 3),
-    pass_MQ = round(mean(MQ > lim.MQ), 3),
-    pass_MQRankSum = round(mean(MQRankSum > lim.MQRankSum), 3),
-    pass_ReadPosRankSum = round(mean(ReadPosRankSum > lim_inf.ReadPosRankSum & ReadPosRankSum < lim_sup.ReadPosRankSum), 3),
-    pass_SOR = round(mean(SOR < lim.SOR), 3),
-    pass_BaseQRankSum = round(mean(BaseQRankSum > lim.BaseQRankSum), 3)
+    pass_QD = round(mean(QD > lim.QD, na.rm = TRUE), 3),
+    pass_FS = round(mean(FS < lim.FS, na.rm = TRUE), 3),
+    pass_MQ = round(mean(MQ > lim.MQ, na.rm = TRUE), 3),
+    pass_MQRankSum = round(mean(if_else(is.na(MQRankSum), TRUE, MQRankSum > lim.MQRankSum), na.rm = TRUE), 3),
+    pass_ReadPosRankSum = round(mean(if_else(is.na(ReadPosRankSum), TRUE, 
+                                             ReadPosRankSum > lim_inf.ReadPosRankSum & 
+                                               ReadPosRankSum < lim_sup.ReadPosRankSum), na.rm = TRUE), 3),
+    pass_SOR = round(mean(SOR < lim.SOR, na.rm = TRUE), 3),
+    pass_BaseQRankSum = round(mean(if_else(is.na(BaseQRankSum), TRUE, BaseQRankSum > lim.BaseQRankSum), na.rm = TRUE), 3)
   )
 print(proportions_pass)
 
-# Filter the data
+# Filter the data, considering NAs
 filtered_data <- annotations %>%
   filter(
     QD > lim.QD,
     FS < lim.FS,
     MQ > lim.MQ,
-    MQRankSum > lim.MQRankSum,
-    ReadPosRankSum > lim_inf.ReadPosRankSum,
-    ReadPosRankSum < lim_sup.ReadPosRankSum,
-    SOR < lim.SOR, 
-    BaseQRankSum > lim.BaseQRankSum
+    (is.na(MQRankSum) | MQRankSum > lim.MQRankSum),
+    (is.na(ReadPosRankSum) | (ReadPosRankSum > lim_inf.ReadPosRankSum & ReadPosRankSum < lim_sup.ReadPosRankSum)),
+    SOR < lim.SOR,
+    (is.na(BaseQRankSum) | BaseQRankSum > lim.BaseQRankSum)
   )
-filtered_data
-cat("Number of rows before filtering:", rows_after, ", after: ", nrow(filtered_data), "\nTotal removed: ", rows_before-nrow(filtered_data))
+
+# Count rows before and after filtering
+rows_after_filtering <- nrow(filtered_data)
+cat("Number of rows before filtering:", rows_after, ", after: ", rows_after_filtering, "\nTotal removed: ", rows_before - rows_after, "\n")
+
+# Group by CHROM and count the number of SNPs for each group
+rows_per_chrom <- filtered_data %>%
+  group_by(CHROM) %>%
+  summarize(n = n())
+print("Number of SNPs for each chromosome after filtration:")
+print(rows_per_chrom)
+
 
 ###### QUAL distribution -----------------------------------------------
 
@@ -102,7 +113,7 @@ QD_plot <- ggplot(annotations, aes(x = QD)) +
   geom_density(fill = "lightblue") +
   geom_vline(xintercept = lim.QD, color = "red") +
   ggtitle(paste0("QD > ", lim.QD, ", pass: ", proportions_pass$pass_QD)) +
-  theme_minimal() +  # Set the minimal theme first
+  theme_minimal() + 
   theme(
     plot.title = element_text(size = 20, hjust = 0.5, face = "bold")  
   )
@@ -128,21 +139,23 @@ MQ_plot <- ggplot(annotations, aes(x = MQ)) +
   )
 MQ_plot
 
+na_mqrs <- sum(is.na(annotations$MQRankSum))
 MQRS_plot <- ggplot(annotations, aes(x = MQRankSum)) +
   geom_density(fill = "lightblue") +
   geom_vline(xintercept = lim.MQRankSum, color = "red") +
-  ggtitle(paste0("MQRankSum > ", lim.MQRankSum,  ", pass: ", proportions_pass$pass_MQRankSum)) +
+  ggtitle(paste0("MQRankSum > ", lim.MQRankSum,  ", pass: ", proportions_pass$pass_MQRankSum, "\n (not counting NA: ",na_mqrs, ")")) +
   theme_minimal() +
   theme(
     plot.title = element_text(size = 20, hjust = 0.5, face = "bold")  
   )
 MQRS_plot
 
+na_rprs <- sum(is.na(annotations$ReadPosRankSum))
 ReadPosRankSum_plot <- ggplot(annotations, aes(x = ReadPosRankSum)) +
   geom_density(fill = "lightblue") +
   geom_vline(xintercept = lim_inf.ReadPosRankSum, color = "red") +
   geom_vline(xintercept = lim_sup.ReadPosRankSum, color = "red") +
-  ggtitle(paste0("ReadPosRankSum: ", lim_inf.ReadPosRankSum, " to ", lim_sup.ReadPosRankSum, ", pass: ", proportions_pass$pass_ReadPosRankSum)) +
+  ggtitle(paste0("ReadPosRankSum: ", lim_inf.ReadPosRankSum, " to ", lim_sup.ReadPosRankSum, ", pass: ", proportions_pass$pass_ReadPosRankSum, "\n (not counting NA: ", na_rprs, ")")) +
   theme_minimal() +
   theme(
     plot.title = element_text(size = 20, hjust = 0.5, face = "bold")  
@@ -159,10 +172,11 @@ SOR_plot <- ggplot(annotations, aes(x = SOR)) +
   )
 SOR_plot
 
+na_bqrs <- sum(is.na(annotations$BaseQRankSum))
 BaseQRankSum_plot <- ggplot(annotations, aes(x = BaseQRankSum)) +
   geom_density(fill = "lightblue") +
   geom_vline(xintercept = lim.BaseQRankSum, color = "red") +
-  ggtitle(paste0("BaseQRankSum > ", lim.BaseQRankSum, ", pass: ", proportions_pass$pass_BaseQRankSum)) +
+  ggtitle(paste0("BaseQRankSum > ", lim.BaseQRankSum, ", pass: ", proportions_pass$pass_BaseQRankSum, "\n (not counting NA: ",na_bqrs, ")")) +
   theme_minimal() +
   theme(
     plot.title = element_text(size = 20, hjust = 0.5, face = "bold")  
@@ -171,10 +185,11 @@ BaseQRankSum_plot
 
 text_grob <- textGrob(
   paste0("Raw dataset: ", rows_before, " SNP                                        \n", 
-         "After removing sites w/ NA: ", rows_after, " SNP               \n", 
-         "After filtration w/ shown thresholds: ", nrow(filtered_data), " SNP"), 
+         "After removing sites w/ NA                 \n
+         (except RankSum scores: ", rows_after, " SNP)               \n", 
+         "After filtration w/ shown thresholds: ", rows_after_filtering, " SNP"), 
   gp = gpar(fontsize = 15, fontface = "bold", col = "black"))
-# Left justification didn't work properly so I wame up with theses spaces
+# Left justification didn't work properly so I came up with theses spaces
 
 combined_plot <- arrangeGrob(
   QD_plot, FS_plot, MQ_plot, MQRS_plot, ReadPosRankSum_plot,
